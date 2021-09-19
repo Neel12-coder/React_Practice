6d805bebb9030d9eebf1a8f4e76e0a340af89dbd
unknown <somaineelam@gmail.com>
2021-09-19 11:12:55 +0530

    const, state, comp api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super(); //super(props) if you want to use props in this cons.
+    this.state = {
+         msg : "WELCOME TO MY STATE"
+      }
+    this.handleUpdateEvent = this.handleUpdateEvent.bind(this); //use arrow function to avoid this
+    this.forceUpdateState = this.forceUpdateState.bind(this);
+  }
+  handleUpdateEvent()
+{
+    this.setState({
+    msg : "WELCOME TO SET STATE UPDATE"
+    });
+};
+
+  forceUpdateState()
+  {
+  this.forceUpdate();
+  };
+
+//arrow function , no need for binding in const
+  findDomNodeHandler = () => {
+  var myDiv = document.getElementById('myDivOne');
+  ReactDOM.findDOMNode(myDiv).style.color = 'red';
+  }
+
+  render() {
+    return (
+      <div className="App">
+    <h2>React Constructor Example</h2>
+    <p>{this.state.msg}</p>
+    <h3>Math = {Math.random()} </h3>
+    <button onClick = {this.findDomNodeHandler}>FIND_DOM_NODE1</button>
+    <h3 id = "myDivOne" >JTP-NODE1</h3>
+        <button onClick={this.handleUpdateEvent}>State Change</button>
+        <button onClick={this.forceUpdateState}>Force State Change</button>
+      </div>
+    );
+  }
 }
-
 export default App;
